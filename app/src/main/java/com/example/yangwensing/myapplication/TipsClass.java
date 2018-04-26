8487b42a2a06f650eb4ec85d61fdef479c8ec08f
yangwensing <a9015633256@hotmail.com>
2018-04-26 17:22:33 +0800

    新增TipsClass記事本

---
+++ app/src/main/java/com/example/yangwensing/myapplication/TipsClass.java
@@ -0,0 +1,65 @@
+package com.example.yangwensing.myapplication;
+
+public class TipsClass {
+
+    /*
+    這是一個記事本，各個Class,Activity,layout,style乃至manifest可以的話把版面的功用新增在這裡
+    Activity區
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    layout區
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    manifest區
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     */
+
+}
