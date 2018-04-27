776fd5a26862db5a7d91da1073e9d6cdbfd8e049
startover205 <startover205@gmail.com>
2018-04-27 12:16:48 +0800

    上傳學生使用介面

--- app/src/main/java/com/example/yangwensing/myapplication/TipsClass.java
+++ app/src/main/java/com/example/yangwensing/myapplication/TipsClass.java
@@ -4,29 +4,67 @@ public class TipsClass {
 
     /*
     這是一個記事本，各個Class,Activity,layout,style乃至manifest可以的話把版面的功用新增在這裡
+
     Activity區
 
+    **classes
+
+
+
+    **main
+
+    MainActivity:放底下導覽列控制、放右上角menu控制
+    BottomNavigationViewHelper:底部導覽列輔助(item超過三個時才不會有浮動動畫)
+    Common:放檢查網路連線的方法、放連接webApp的URL位址、放
+    MyTask:開子執行緒連接web **尚未測試
+    Subject:科目類別 **目前無用途
+    Teacher:老師類別 **目前無用途
+
+
+    **info
+
+    StudentInfoFragment:學生個人資料
+    StudentInfoEditFragment:學生個人資料編輯
+
+    **homework
+
+    StudentHomeworkFragment:學生作業檢視
+    Homework:作業類別
+    AssignDate:rv用的作業類別，按照日期分類
+
+    **exam
+
+    StudentExamFragment:學生成績檢視
+
+
+
+    **contact
+    StudentContactFragment:學生用 老師列表檢視
+    StudentContactDetailFragment:學生用 老師詳細檢視
+    Teacher:列表用的老師列表，之後會刪除改用mainPackage裡的
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    -----------
 
     layout區
 
+    activity_main:有放底部導覽列
+
+    fragment_student_info:學生個人資料
+    fragment_student_info_edit:學生個人資料編輯
+    fragment_student_homework:學生作業檢視
+    fragment_student_homework_detail:學生作業檢視的細項的格式
+    fragment_student_exam:學生成績檢視
+    fragment_student_contact:學生用 老師列表檢視
+    fragment_student_contact_detail:學生用 老師詳細資料檢視
+
+    cardview_homework:rv用
+    cardview_homework_detail:暫無用途
+    cardview_exam:rv用
+    cardview_contact:rv用
 
 
 
@@ -36,13 +74,7 @@ public class TipsClass {
 
 
 
-
-
-
-
-
-
-
+    -----------
 
     manifest區
 
