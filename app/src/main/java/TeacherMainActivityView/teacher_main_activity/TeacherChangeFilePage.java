dbeaaeec548f454a7afd226cdaa2677b4bc33244
yangwensing <a9015633256@hotmail.com>
2018-05-07 18:52:53 +0800

    文興的部分新增

---
+++ app/src/main/java/TeacherMainActivityView/teacher_main_activity/TeacherChangeFilePage.java
@@ -0,0 +1,16 @@
+package TeacherMainActivityView.teacher_main_activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.yangwensing.myapplication.R;
+
+public class TeacherChangeFilePage extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.teacher_change_file);
+        setTitle(R.id.Teacher_ChangeFile);
+    }
+}
