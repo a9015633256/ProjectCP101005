0de2b66e5e80be0082f6fe56b992fb8ff065430b
nameless <tim45211@hotmail.com>
2018-05-11 17:56:11 +0800

    2018.5.11聊天雛形 完成

--- app/src/main/java/com/example/yangwensing/myapplication/chat/ChatFragment.java
+++ app/src/main/java/com/example/yangwensing/myapplication/chat/ChatFragment.java
@@ -60,9 +60,10 @@ public class ChatFragment extends Fragment {
     private LinearLayout layout;
     private ScrollView scrollView;
     private MyTask messageTask;
-    private TextView tvMessage;
+
     private Button btsend;
     private EditText etmessage;
+
 
 
     @Nullable
@@ -73,7 +74,7 @@ public class ChatFragment extends Fragment {
 
         layout = view.findViewById(R.id.layout);
         scrollView = view.findViewById(R.id.scrollView);
-        tvMessage= view.findViewById(R.id.tvMessage);
+
         btsend = view.findViewById(R.id.btSend);
         etmessage = view.findViewById(R.id.etMessage);
 
@@ -110,16 +111,39 @@ public class ChatFragment extends Fragment {
         } catch (Exception e) {
             Log.e(TAG, e.toString());
         }
+
         if (messages == null || messages.isEmpty()) {
             Toast.makeText(getActivity(), "empty", Toast.LENGTH_SHORT).show();
 
         } else {
+            String sender = senderte;
+            View viewr;
             Toast.makeText(getActivity(), "have message", Toast.LENGTH_SHORT).show();
-//            String me ="";
-//            for (int i = 0; i <= messages.size(); i++){
-//                me = messages.get(i).getMessage() ;
-//            }
-//            tvMessage.setText(me);
+            for (int i=1; i<messages.size(); i++) {
+                if (senderte.equals(messages.get(i).getSender())){
+                    String text = sender + ":\n " + messages.get(i).getMessage() + "\n";
+
+
+                    viewr = View.inflate(getActivity(), R.layout.chat_message_right, null);
+                    TextView textView = viewr.findViewById(R.id.tvtype);
+                    textView.setText(text);
+                    layout.addView(viewr);
+                }else{
+                    String text =receiver + ":\n " + messages.get(i).getMessage() + "\n";
+
+                    viewr = View.inflate(getActivity(), R.layout.chat_message_left, null);
+                    TextView textView = viewr.findViewById(R.id.tvtype);
+                    textView.setText(text);
+                    layout.addView(viewr);
+                }
+                scrollView.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        scrollView.fullScroll(View.FOCUS_DOWN);//自動捲動到最下面
+                    }
+                });
+
+            }
 
         }
 
@@ -132,10 +156,12 @@ public class ChatFragment extends Fragment {
                     Toast.makeText(getActivity(), "empty", Toast.LENGTH_SHORT).show();
                     return;
                 }
+
                 String sender = senderte;
                 // 將欲傳送訊息先顯示在TextView上
 
-                tvMessage.append(sender + ": " + message + "\n");
+                showMessage(sender, message, false);
+
                 // 將輸入的訊息清空
                 etmessage.setText(null);
                 // 捲動至最新訊息
@@ -148,7 +174,6 @@ public class ChatFragment extends Fragment {
                 // 將欲傳送訊息轉成JSON後送出
                 ChatMessage chatMessage = new ChatMessage("chat", sender, receiver, message);
                 String chatMessageJson = new Gson().toJson(chatMessage);
-                Log.d(TAG, "output: " + chatMessageJson);
                 chatWebSocketClient.send(chatMessageJson);
                 Log.d(TAG, "output: " + chatMessageJson);
 
@@ -173,36 +198,31 @@ public class ChatFragment extends Fragment {
             String sender = chatMessage.getSender();
             // 接收到聊天訊息，若發送者與目前聊天對象相同，就將訊息顯示在TextView
             if (sender.equals(receiver)) {
-                tvMessage.append(sender + ": " + chatMessage.getMessage() + "\n");
-                scrollView.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        scrollView.fullScroll(View.FOCUS_DOWN);
-                    }
-                });
+                showMessage(sender, chatMessage.getMessage(), true);
+
             }
             Log.d(TAG, message);
         }
     }
 
-//    private void showMessage(String sender, String message, boolean left) {
-//        String text = sender + ": " + message;
-//        View view;
-//        // 準備左右2種layout給不同種類發訊者(他人/自己)使用
-//        if (left) {
-//            view = View.inflate(getActivity(), R.layout.chat_message_left, null);
-//
-//        } else {
-//            view = View.inflate(getActivity(), R.layout.chat_message_right, null);
-//        }
-//        TextView textView = view.findViewById(R.id.textView);
-//        textView.setText(text);
-//        layout.addView(view);
-//        scrollView.post(new Runnable() {
-//            @Override
-//            public void run() {
-//                scrollView.fullScroll(View.FOCUS_DOWN);//自動捲動到最下面
-//            }
-//        });
-//    }
+    private void showMessage(String sender, String message, boolean left) {
+        String text = sender + ":\n " + message;
+        View viewr;
+        // 準備左右2種layout給不同種類發訊者(他人/自己)使用
+        if (left) {
+            viewr = View.inflate(getActivity(), R.layout.chat_message_left, null);
+
+        } else {
+            viewr = View.inflate(getActivity(), R.layout.chat_message_right, null);
+        }
+        TextView textView = viewr.findViewById(R.id.tvtype);
+        textView.setText(text);
+        layout.addView(viewr);
+        scrollView.post(new Runnable() {
+            @Override
+            public void run() {
+                scrollView.fullScroll(View.FOCUS_DOWN);//自動捲動到最下面
+            }
+        });
+    }
 }
